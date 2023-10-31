d912ad937495256eaa4f714601eccf99283c8abe
Omar Al Zoubi <129740652+Omar-WD@users.noreply.github.com>
2023-10-31 23:51:55 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,28 +4,30 @@ const express = require('express');
 require("./db")
 const {errorHandler}= require("./middleware/errorHandles")
 const cookieParser = require("cookie-parser");
+const path = require('path');
+const app = express();
 const cors = require("cors")
 const port =process.env.PORT || 3000
-
 const productRouter=require("./routers/products");
 const authRouter = require("./routers/auth");
 
+
+
+
+
+
 app.use(express.static(path.join(__dirname, "client", "dist")))
 
-
-
-
-const app = express();
 app.use(cookieParser());
 app.use(express.json())
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: 'https://elzoobiautohandel.onrender.com',
     credentials: true
 }))
 
 
-app.use("/user",authRouter)
-app.use("/products",productRouter)
+app.use("/api/user",authRouter)
+app.use("/api/products",productRouter)
 
 app.get("*", (req, res)=>{
     res.sendFile(path.join(__dirname, "client", "dist", "index.html" ))
